18cb38e558a8617890f0dd1d55e29079b6d4ddbc
ajay-chebrolu <s190228@rguktsklm.ac.in>
2023-02-24 12:29:49 +0530

    notes

---
+++ notes/week0/return.py
@@ -0,0 +1,9 @@
+def main():
+	x = int(input("What's x? "))
+	print("x square is",square(x))
+
+# Square function
+def square(n):
+	return n * n;
+
+main()
---
+++ notes/week0/round1.py
@@ -0,0 +1,7 @@
+# round(number[, ndigits])
+x = float(input("What's x? "))
+y = float(input("What' y? "))
+
+z = round(x+y)
+
+print(f"{z:.2f}")
