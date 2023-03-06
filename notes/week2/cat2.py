4676e9e322d1ef9ec8f84faddf55efea84972baa
ajay-chebrolu <s190228@rguktsklm.ac.in>
2023-03-06 18:42:52 +0530

    notes

---
+++ notes/week2/cat2.py
@@ -0,0 +1,2 @@
+num = int(input("Enter number : "))
+print("meow\n"* num,end='')
---
+++ notes/week2/cat4.py
@@ -0,0 +1,15 @@
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            return n
+
+def meow(number):
+    for _ in range(number):
+        print("meow")
+
+main()
