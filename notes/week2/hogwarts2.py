5d3edab1fe689db0c2b6fc55442d015995dbbfbd
ajay-chebrolu <s190228@rguktsklm.ac.in>
2023-03-08 20:09:26 +0530

    notes

---
+++ notes/week2/hogwarts2.py
@@ -0,0 +1,12 @@
+students = {
+    "Hermoine":"Griffindor",
+    "Harry":"Griffindor",
+    "Ron":"Griffindor",
+    "Draco":"Slytherin"
+}
+# print(students["Hermoine"]) - output Griffindor
+
+for student in students:
+    print(student, students[student])
+# student - key
+# students[student] - value
