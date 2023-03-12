8a47f8cf0e911419ba365f05c3878976236cccdf
ajay-chebrolu <s190228@rguktsklm.ac.in>
2023-03-12 11:13:56 +0530

    notes

---
+++ notes/week3/number.py
@@ -0,0 +1,6 @@
+# Catches a ValueError
+try:
+    x = int(input("What's x? "))
+    print(f"x is {x}")
+except ValueError:
+    print("x is not an integer")
---
+++ notes/week3/number1.py
@@ -0,0 +1,7 @@
+# Demonstrate NameError
+try:
+    x = int(input("What's x? "))
+except ValueError:
+    print("x is not an integer")
+
+print(f"x is {x}")
