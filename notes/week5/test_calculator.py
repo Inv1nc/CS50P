2183a137b844c962047776a4c7a50079279a89e5
ajay-chebrolu <s190228@rguktsklm.ac.in>
2023-03-17 17:59:50 +0530

    notes

---
+++ notes/week5/test_calculator.py
@@ -0,0 +1,8 @@
+from calculator import square
+
+
+def test_square():
+    if square(2) != 4:
+        print("2 square was not 4")
+    if square(3) != 9:
+        print("3 square was not 9")
---
+++ notes/week5/test_calculator3.py
@@ -0,0 +1,20 @@
+from calculator import square
+
+
+def test_positive():
+    assert square(1) == 1
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(4) == 16
+    assert square(5) == 25
+
+def test_negative():
+    assert square(-1) == 1
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(-4) == 16
+    assert square(-5) == 25
+
+
+def test_zero():
+    assert square(0) == 0
